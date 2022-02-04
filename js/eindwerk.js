b5c0d006d4e1ca184200d5d89c576fc061c4f214
Ienne <daemenienne@gmail.com>
2022-02-04 12:06:18 +0100

    add index tabs

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -1,5 +1,22 @@
-$(document).ready(function () {
+$(document).ready(function ()
+{
 	$('#accordion').accordion({
 		collapsible: true,
 	});
+
+
+
+
+
+
+
+
+
+	//IENNE
+	$("#tabs").tabs({
+		active: 0,
+		show: { effect: "blind", duration: 250 },
+		hide: { effect: "blind", duration: 250 },
+	});
+	//IENNE END
 });
