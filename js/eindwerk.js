0a76acae5d7d8b803bd811ca24d9f860d588394d
Teun vd Palen <teun.vd.palen@gmail.com>
2022-02-08 10:56:22 +0100

    Content load in en  style form

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -1,25 +1,40 @@
-$(document).ready(function ()
-{
+$(document).ready(function () {
+	// Begin code Teun
+	// accordion jquery pagina
 	$('#accordion').accordion({
 		collapsible: true,
+		icons: { header: 'ui-icon-plus', activeHeader: 'ui-icon-minus' },
 	});
 
+	// Index pagina inladen met andere paginas
 
+	let $content = $('#content');
 
+	$('.main-nav ul li > a').on('click', function (e) {
+		let toLoad = $(this).attr('href');
+		$content.hide('fast', loadContent);
 
+		function loadContent() {
+			console.log(toLoad);
+			$content.load(toLoad, showNewContent);
+		}
+		function showNewContent() {
+			$content.show('normal');
+		}
 
+		e.preventDefault();
+	});
 
-
-
+	// Einde code Teun
 
 	//IENNE
 
 	// --TABS
-	let $tabs = $("#tabs");
+	let $tabs = $('#tabs');
 	$tabs.tabs({
 		active: 0,
-		show: { effect: "blind", duration: 250 },
-		hide: { effect: "blind", duration: 250 },
+		show: { effect: 'blind', duration: 250 },
+		hide: { effect: 'blind', duration: 250 },
 	});
 
 	// --SEARCH
@@ -29,98 +44,70 @@ $(document).ready(function ()
 	let averageSpeed = 600;
 	let speed = 900;
 
-	let autocomplete =
-		[
-			'Basis',
-			'Events',
-			'Effecten',
-			'jQuery UI',
-			'Animatie',
-			'Plugin',
-			'Externe data',
-			'Home',
-			'jQuery',
-			'Contact',
-			'Datums',
-			'Voor wie',
-			'Over de cursus'
-		]
+	let autocomplete = ['Basis', 'Events', 'Effecten', 'jQuery UI', 'Animatie', 'Plugin', 'Externe data', 'Home', 'jQuery', 'Contact', 'Datums', 'Voor wie', 'Over de cursus'];
 
-	let links =
-		[
-			'jquery.html#basis',
-			'jquery.html#events',
-			'jquery.html#effecten',
-			'jquery.html#jquery-ui',
-			'jquery.html#animatie',
-			'jquery.html#plugin',
-			'jquery.html#externe-data',
-			'index.html',
-			'jquery.html',
-			'contact.html',
-			'index.html#tabs 2',
-			'index.html#tabs 1',
-			'index.html#tabs 0'
-		]
+	let links = [
+		'jquery.html#basis',
+		'jquery.html#events',
+		'jquery.html#effecten',
+		'jquery.html#jquery-ui',
+		'jquery.html#animatie',
+		'jquery.html#plugin',
+		'jquery.html#externe-data',
+		'index.html',
+		'jquery.html',
+		'contact.html',
+		'index.html#tabs 2',
+		'index.html#tabs 1',
+		'index.html#tabs 0',
+	];
 
-	$('.fa-search').on('click', function ()
-	{
-		if (!open)
-		{
+	$('.fa-search').on('click', function () {
+		if (!open) {
 			open = true;
 			speed = ((150 - $inputSearch.width()) / 100) * averageSpeed;
 			$inputSearch.stop();
 
-			$icon.addClass('search-icon-active', 100, function ()
-			{
-				$inputSearch.animate({
-					width: '150px',
-					padding: '0, 15px',
-				}, speed)
+			$icon.addClass('search-icon-active', 100, function () {
+				$inputSearch.animate(
+					{
+						width: '150px',
+						padding: '0, 15px',
+					},
+					speed
+				);
 			});
-		}
-		else
-		{
+		} else {
 			Search();
-			if ($inputSearch.val() == "")
-				CloseSearch();
-
+			if ($inputSearch.val() == '') CloseSearch();
 		}
 	});
 
-	$inputSearch.on('keypress', function (e)
-	{
+	$inputSearch.on('keypress', function (e) {
 		// 13 is the ENTER key
-		if (e.which == 13) 
-		{
+		if (e.which == 13) {
 			Search();
 		}
-	})
+	});
 
 	$inputSearch.autocomplete({
 		source: autocomplete,
 		autoFocus: true,
 		delay: 600,
-	})
+	});
 
-	function Search()
-	{
-		if ($inputSearch.val() != "")
-		{
-			for (let i = 0; i < autocomplete.length; ++i)
-			{
+	function Search() {
+		if ($inputSearch.val() != '') {
+			for (let i = 0; i < autocomplete.length; ++i) {
 				//TODO extra bijzetten op het einde
-				if ($inputSearch.val() == autocomplete[i])
-				{
-					if (links[i].includes('index.html#tabs'))
-					{
+				if ($inputSearch.val() == autocomplete[i]) {
+					if (links[i].includes('index.html#tabs')) {
 						let idx = links[i].substring(links[i].indexOf(' '), links[i].length);
 						console.log(idx);
 
 						window.location.href = links[i];
-						$tabs.tabs("option", "active", idx);
-					}
-					else window.location.href = links[i];
+						$tabs.tabs('option', 'active', idx);
+					} else window.location.href = links[i];
 
 					CloseSearch();
 				}
@@ -128,28 +115,28 @@ $(document).ready(function ()
 		}
 	}
 
-	function CloseSearch()
-	{
-		if (open)
-		{
+	function CloseSearch() {
+		if (open) {
 			speed = ($inputSearch.width() / 100) * averageSpeed;
 
 			$inputSearch.stop();
 
-			$inputSearch.animate({
-				width: '0px',
-				padding: '0',
-			}, speed, function ()
-			{
-				$inputSearch.hide();
-				$icon.removeClass('search-icon-active', 100);
-				$inputSearch.val("");
-			});
+			$inputSearch.animate(
+				{
+					width: '0px',
+					padding: '0',
+				},
+				speed,
+				function () {
+					$inputSearch.hide();
+					$icon.removeClass('search-icon-active', 100);
+					$inputSearch.val('');
+				}
+			);
 
 			open = false;
 		}
 	}
-
 
 	//IENNE END
 });
